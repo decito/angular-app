49414a58c77c27b9444bb491434e58d2306a118e
Decio Neto <deciom.neto10@gmail.com>
2023-03-09 18:42:17 -0300

    feat: add style dummy data to components

--- src/app/components/recipes/recipe-list/recipe-list.component.ts
+++ src/app/components/recipes/recipe-list/recipe-list.component.ts
@@ -1,9 +1,26 @@
 import { Component } from '@angular/core';
 
+import { Recipe } from 'src/app/models/recipe/recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html'
 })
 export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Dummy Recipe',
+      'This is a dummy recipe.',
+      'http://www.alittletipsy.com/wp-content/uploads/2014/05/The-Best-Hambuger-Recipe-Mozzarella-Burgers-square.jpg'
+    ),
+    new Recipe(
+      'Dummy Recipe',
+      'This is a dummy recipe.',
+      'http://www.alittletipsy.com/wp-content/uploads/2014/05/The-Best-Hambuger-Recipe-Mozzarella-Burgers-square.jpg'
+    ),
+  ]
 
+  constructor() { }
+
+  ngOnInit() { }
 }
