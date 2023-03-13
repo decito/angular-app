253469cf9fb7043f628809dd1482516446bcc522
Decio Neto <deciom.neto10@gmail.com>
2023-03-13 16:38:53 -0300

    feat: add current route decorator

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from "@angular/core";
+import { Component, EventEmitter, Output } from "@angular/core"
 
 @Component({
   selector: 'app-header',
@@ -6,8 +6,19 @@ import { Component, EventEmitter, Output } from "@angular/core";
 })
 export class HeaderComponent {
   @Output() curRoute = new EventEmitter<string>()
+  intRoute: string = 'recipes'
 
   changeRoute(route: string) {
+    this.intRoute = route
+
     this.curRoute.emit(route)
+  }
+
+  isRecipeCurRoute() {
+    return this.intRoute === 'recipes'
+  }
+
+  isShoppingCurRoute() {
+    return this.intRoute === 'shopping'
   }
 }
