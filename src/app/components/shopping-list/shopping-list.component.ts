65d77ab45c9e79922d56f1afb4f7a6abe2646b21
Decio Neto <deciom.neto10@gmail.com>
2023-03-13 14:22:15 -0300

    feat: add shopping list content

--- src/app/components/shopping-list/shopping-list.component.ts
+++ src/app/components/shopping-list/shopping-list.component.ts
@@ -1,9 +1,14 @@
 import { Component } from "@angular/core";
+import { Ingredient } from "src/app/models/shopping-list/ingredient.model";
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html'
 })
 export class ShoppingListComponent {
-
+  ingredients: Ingredient[] = [
+    new Ingredient('Tomatoes', 7),
+    new Ingredient('Eggs', 8),
+    new Ingredient('Onions', 9),
+  ]
 }
