cb89e8b04a294bd46eb3b80b85da8194b20248e6
Decio Neto <deciom.neto10@gmail.com>
2023-03-16 08:58:56 -0300

    fix: fix header on mobile

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -26,4 +26,44 @@ export class DropdownDirective {
       }
     }
   }
+
+  // cleanAttr(el: HTMLElement) {
+  //   const allElements = document.getElementsByTagName('*')
+
+  //   for (let i = 0; i < allElements.length; i++) {
+  //     const element = allElements[i]
+
+  //     if (element !== el && el.contains(element)) {
+  //       element.removeAttribute('open')
+  //     }
+  //   }
+  // }
+
+  // @HostListener('document:click', ['$event']) onClick(event: MouseEvent) {
+  //   let el = event.target as HTMLElement
+
+  //   while (!el.hasAttribute('toggleController')) {
+  //     if (el.tagName === maxNode) {
+  //       this.cleanAttr(el)
+  //       return
+  //     }
+
+  //     el = el.parentNode as HTMLElement
+  //   }
+
+
+  //   el.childNodes.forEach(childNode => {
+  //     if (childNode.nodeType === Node.ELEMENT_NODE) {
+  //       const element = childNode as HTMLElement
+
+  //       if (element.hasAttribute('toggle') && element.hasAttribute('open'))
+  //         element.removeAttribute('open')
+
+  //       else if (element.hasAttribute('toggle'))
+  //         element.setAttribute('open', '')
+
+  //       el = element
+  //     }
+  //   })
+  // }
 }
