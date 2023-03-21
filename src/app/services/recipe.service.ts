54ba0c6106375867a080e2ae9766b368c082e02e
Decio Neto <deciom.neto10@gmail.com>
2023-03-21 10:45:19 -0300

    feat: add recipe service

---
+++ src/app/services/recipe.service.ts
@@ -0,0 +1,25 @@
+import { EventEmitter, Injectable } from "@angular/core"
+
+import { Recipe } from "src/app/models/recipe/recipe.model"
+
+@Injectable({ providedIn: 'root' })
+export class RecipeService {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Dummy Recipe 1',
+      'This is the first dummy recipe.',
+      'http://www.alittletipsy.com/wp-content/uploads/2014/05/The-Best-Hambuger-Recipe-Mozzarella-Burgers-square.jpg'
+    ),
+    new Recipe(
+      'Dummy Recipe 2',
+      'This is the second dummy recipe.',
+      'http://www.alittletipsy.com/wp-content/uploads/2014/05/The-Best-Hambuger-Recipe-Mozzarella-Burgers-square.jpg'
+    ),
+  ]
+
+  getRecipes() {
+    return this.recipes.slice()
+  }
+
+  recipeSelected = new EventEmitter<Recipe>()
+}
