e927346dc651cf433e8b0008f1c9942a3e7fc512
Decio Neto <deciom.neto10@gmail.com>
2023-04-05 15:08:54 -0300

    chore: add routerlink to recipe item

--- src/app/components/recipes/recipe-item/recipe-item.component.ts
+++ src/app/components/recipes/recipe-item/recipe-item.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core'
 
+import { RecipesService } from '~/services/recipes.service'
+
 import { Recipe } from '~/models/recipe/recipe.model'
 
 @Component({
@@ -8,4 +10,11 @@ import { Recipe } from '~/models/recipe/recipe.model'
 })
 export class RecipeItemComponent {
   @Input() item: Recipe
+  @Input() index: number
+
+  constructor(private recipesService: RecipesService) {}
+
+  selectRecipe() {
+    this.recipesService.recipeSelected.emit(this.item)
+  }
 }
