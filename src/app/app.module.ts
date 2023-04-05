0c4cb91dc7b3b79ff5e60b56f85a427fa05e55ba
Decio Neto <deciom.neto10@gmail.com>
2023-04-05 12:05:40 -0300

    feat: add and use app base routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 import { FormsModule } from '@angular/forms'
 
-import { AppRoutingModule } from './app-routing.module'
+import { AppRoutingModule } from './routes/app-routing.module'
 
 import { AppComponent } from './app.component'
 
@@ -11,6 +11,7 @@ import { DropdownDirective } from './shared/dropdown.directive'
 import { HtmlCommentDirective } from './shared/html-comment.directive'
 import { UnlessDirective } from './shared/unless.directive'
 
+import { ErrorPageComponent } from './components/error-page/error-page.component'
 import { HeaderComponent } from './components/header/header.component'
 import { RecipeDetailComponent } from './components/recipes/recipe-detail/recipe-detail.component'
 import { RecipeItemComponent } from './components/recipes/recipe-item/recipe-item.component'
@@ -24,6 +25,7 @@ import { ShoppingListEditComponent } from './components/shopping-list/shopping-l
     AppComponent,
     BasicHighlightDirective,
     DropdownDirective,
+    ErrorPageComponent,
     HeaderComponent,
     HtmlCommentDirective,
     RecipeDetailComponent,
---
+++ src/app/components/error-page/error-page.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core'
+import { ActivatedRoute, Data } from '@angular/router'
+
+@Component({
+  selector: 'app-error-page',
+  templateUrl: './error-page.component.html'
+})
+export class ErrorPageComponent implements OnInit {
+  errorMessage: string
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.route.data.subscribe((d: Data) => (this.errorMessage = d['message']))
+  }
+}
