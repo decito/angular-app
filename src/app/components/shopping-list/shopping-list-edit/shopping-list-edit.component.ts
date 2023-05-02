e17c4b2aa6b03e8a759e878c0d4d2ca6d87c9bc6
Decio Neto <deciom.neto10@gmail.com>
2023-05-02 14:48:12 -0300

    feat: use angular form in shoppingListEdit component

--- src/app/components/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
+++ src/app/components/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
@@ -1,22 +1,29 @@
-import { Component, ElementRef, ViewChild } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
 
 import { ShoppingService } from '~/services/shopping.service'
 
 import { Ingredient } from '~/models/shopping-list/ingredient.model'
+import { FormControl, FormGroup, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-shopping-list-edit',
   templateUrl: './shopping-list-edit.component.html'
 })
-export class ShoppingListEditComponent {
-  @ViewChild('nameInput') nameInputRef: ElementRef
-  @ViewChild('amountInput') amountInputRef: ElementRef
+export class ShoppingListEditComponent implements OnInit {
+  ingredientForm: FormGroup
 
   constructor(private shoppingService: ShoppingService) {}
 
+  ngOnInit(): void {
+    this.ingredientForm = new FormGroup({
+      name: new FormControl(null, Validators.required),
+      amount: new FormControl(null, [Validators.required, Validators.min(1)])
+    })
+  }
+
   onAddItem() {
-    const ingName = this.nameInputRef.nativeElement.value
-    const ingAmount = this.amountInputRef.nativeElement.value
+    const ingName = this.ingredientForm.value['name']
+    const ingAmount = this.ingredientForm.value['amount']
     const newIngredient = new Ingredient(ingName, ingAmount)
 
     this.shoppingService.addIngredient(newIngredient)
