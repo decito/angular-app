92f31c8d9cf265d5f32281f118b5e1ef87652d06
decito <deciom.neto10@gmail.com>
2023-05-18 16:03:29 -0300

    chore: add subscription to auto-update list

--- src/app/components/recipes/recipe-list/recipe-list.component.ts
+++ src/app/components/recipes/recipe-list/recipe-list.component.ts
@@ -1,23 +1,33 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnDestroy, OnInit } from '@angular/core'
 
 import { RecipesService } from '~/services/recipes.service'
 
 import { Recipe } from '~/models/recipe/recipe.model'
+import { Subscription } from 'rxjs/internal/Subscription'
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html'
 })
-export class RecipeListComponent implements OnInit {
+export class RecipeListComponent implements OnInit, OnDestroy {
   recipes: Recipe[]
+  subscription: Subscription
 
   constructor(private RecipesService: RecipesService) {}
 
   ngOnInit(): void {
+    this.subscription = this.RecipesService.recipeUpdated.subscribe(
+      (r: Recipe[]) => (this.recipes = r)
+    )
+
     this.recipes = this.RecipesService.getRecipes()
   }
 
   onHideTemplate() {
     this.RecipesService.hideTemplate.next(true)
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe()
   }
 }
